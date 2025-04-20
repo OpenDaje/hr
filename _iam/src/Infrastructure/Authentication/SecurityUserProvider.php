a504e2e0fbf6528fe3b150f41c6e4d4373c46cb4
zero <teclaizerai@gmail.com>
2025-04-20 17:00:47 +0200

    Fix CS.

--- _iam/src/Infrastructure/Authentication/SecurityUserProvider.php
+++ _iam/src/Infrastructure/Authentication/SecurityUserProvider.php
@@ -64,7 +64,7 @@ class SecurityUserProvider implements UserProviderInterface, PasswordUpgraderInt
     public function refreshUser(UserInterface $user): UserInterface
     {
         if (! $user instanceof SecurityUser) {
-            throw new UnsupportedUserException(sprintf('Invalid user class "%s".', $user::class));
+            throw new UnsupportedUserException(\sprintf('Invalid user class "%s".', $user::class));
         }
 
         // Return a User object after making sure its data is "fresh".
